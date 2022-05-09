4edc88321da94d1a1f7bcaaa1f0cddfe2334b17e
SCHWABL Herbert <herbert.schwabl@frequentis.com>
2022-05-09 14:04:11 +0200

    polishing

--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -1,18 +1,8 @@
 package com.example
 
-import io.ktor.server.routing.*
-import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.plugins.*
-import io.ktor.server.application.*
-import io.ktor.server.response.*
-import io.ktor.server.request.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
 import kotlin.test.*
 import io.ktor.server.testing.*
 import com.example.plugins.*
-import kotlinx.coroutines.delay
 import java.lang.Thread.sleep
 
 class ApplicationTest {
@@ -20,6 +10,7 @@ class ApplicationTest {
     fun testRoot() = testApplication {
         application {
             configureMessageQueue()
+            configureSerialization()
             sleep(5000)
         }
         MessageProducer().run()
